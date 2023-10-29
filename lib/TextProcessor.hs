47e6124bde562502a5c55778883224f744d3ce51
afmika <afmichael73@gmail.com>
2023-10-29 21:09:45 +0300

    feat: add symb & list parser

--- lib/TextProcessor.hs
+++ lib/TextProcessor.hs
@@ -6,10 +6,11 @@ import System.Environment
 
 data FgValue = Literal String
     | List [FgValue]                    -- [v1, v2, ..]
-    | ListGenerator FgValue FgValue     -- v1 .. v2
     | Number Float                      -- 1234, 1.65 ..
     | String String                     -- "(.+)"
     | Bool Bool                         -- Literal false | true -> Bool false | true
+    | Operator String                   -- +, -, /, *, not, in, list generator .., ..
+    | Symbol String                     -- (, ), {, }, ..
     deriving (Show, Eq)
 
 parseString :: Parser FgValue
@@ -22,28 +23,67 @@ parseString =
         where
             escaped = "\""
 
-
 parseLiteral :: Parser FgValue
 parseLiteral =
     let underscore = char '_' in do
-        start <- letter <|> underscore -- cannot start with a number
-        rest <- many (letter <|> digit <|> underscore)
-        let matched = start:rest
-        return $ case matched of
+        head <- letter <|> underscore -- cannot start with a number
+        tail <- many (letter <|> digit <|> underscore)
+        let literal = head:tail
+        return $ case literal of
             "false" -> Bool False
             "true"  -> Bool True
-            _       -> Literal matched
+            _       -> Literal literal
 
 
 parseNumber :: Parser FgValue
--- liftM (Number . read) $ many1 digit
+-- equiv. liftM (Number . read) $ many1 digit
 parseNumber = Number . read <$> many1 digit
+
+parseList :: Parser FgValue
+parseList =
+        do
+            -- TODO: nested & key: value syntax
+            char '['
+            items <- endBy parseExpr (many space >> many (char ',')) -- <expr> *,
+            char ']'
+            return $ List items
+
+
+symbol :: Parser Char
+symbol = oneOf "+-/*%<>=(){}"
+
+parseSymbol :: Parser FgValue
+parseSymbol =
+        let symbolAsString = fmap (:[]) symbol
+        in do
+            match <- string ".."
+                <|> string ">="
+                <|> string "<="
+                <|> string "=="
+                <|> symbolAsString
+            -- TODO: refactor
+            return $ case match of
+                ".." -> Operator ".."
+                "==" -> Operator "=="
+                "+" -> Operator "+"
+                "-" -> Operator "-"
+                "/" -> Operator "/"
+                "*" -> Operator "*"
+                "=" -> Operator "="
+                "%" -> Operator "%"
+                ">=" -> Operator ">="
+                ">" -> Operator ">"
+                "<=" -> Operator "<="
+                "<" -> Operator "<"
+                s -> Symbol s
 
 
 parseExpr :: Parser FgValue
 parseExpr = parseLiteral
     <|> parseString
     <|> parseNumber
+    <|> parseSymbol
+    <|> parseList
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "unexpected token!" input of
