630dba7cad4f408bfae7eda1aa2440e777afe85c
afmika <afmichael73@gmail.com>
2023-10-31 22:53:46 +0300

    refactor: add lexeme logic

--- lib/TextProcessor.hs
+++ lib/TextProcessor.hs
@@ -2,61 +2,61 @@ module TextProcessor where
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
-import Text.Parsec.Token (GenTokenParser(charLiteral, decimal))
 import Data.List.NonEmpty (some1)
 import qualified Control.Applicative as Parsec
 import qualified Data.Foldable as Parsec
 import GHC.Float
 
 import FgAST
+import Control.Monad
+
+whitespace :: Parser ()
+whitespace = void $ many space
+
+lexeme :: Parser a -> Parser a
+lexeme p = p <* whitespace
 
 parseString :: Parser FgValue
-parseString =
-        do
-            char '"'
-            x <- many (noneOf escaped)
-            char '"'
-            return $ String x
-        where
-            escaped = "\""
+parseString = do
+    lexeme $ char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
 
 parseLiteral :: Parser FgValue
-parseLiteral =
-    let underscore = char '_' in do
-        head <- letter <|> underscore -- cannot start with a number
-        tail <- many (letter <|> digit <|> underscore)
-        let literal = head:tail
-        return $ case literal of
-            "false" -> Bool False
-            "true"  -> Bool True
-            _       -> Literal literal
+parseLiteral = let underscore = char '_' in do
+    head <- letter <|> underscore -- cannot start with a number
+    tail <- many (letter <|> digit <|> underscore)
+    let literal = head:tail
+    return $ case literal of
+        "false" -> Bool False
+        "true"  -> Bool True
+        _       -> Literal literal
 
 
 {- NUMBERS -}
 
 parseDigits :: Parser FgValue
 -- equiv. liftM (Number . read) $ many1 digit
-parseDigits = Number . read <$> many1 digit
+parseDigits = Number . read <$> lexeme (many1 digit)
 
 parseFloat :: Parser FgValue
-parseFloat =
-    do
-        first <- parseDigits
-        char '.'
-        rem <- many digit
-        let Number n =  first
-        let exp = int2Double $ length rem
-        let d = read rem
-        return $ Number (n + (d / (10 ** exp)))
+parseFloat = do
+    first <- parseDigits
+    char '.'
+    rem <- many digit
+    let Number n =  first
+    let exp = int2Double $ length rem
+    let d = read rem
+    return $ Number (n + (d / (10 ** exp)))
 
 parseNumber :: Parser FgValue
-parseNumber =
-    do
-        let neg = do
-                char '-'
-                Number x <- positiveNumber
-                return $ Number (-x)
-        try neg <|> try positiveNumber
+parseNumber = do
+    let neg = do
+            char '-'
+            Number x <- positiveNumber
+            return $ Number (-x)
+    try neg <|> try positiveNumber
     where
         positiveNumber = try parseFloat <|> parseDigits
 
@@ -64,114 +64,106 @@ parseNumber =
 {- TUPLES/LIST -}
 
 parseTupItem :: Parser (FgValue, FgValue)
-parseTupItem =
-    let _s = many space in do
-    _s
-    item <- parseExpr
-    _s
-    -- option (char ',') ?? 
-    many (char ',')
+parseTupItem = let _s = whitespace in do
+    item <- lexeme parseExpr
+    lexeme $ many (char ',')
     return (Number 0, item)
 
 
 parseTupKeyValue :: Parser (FgValue, FgValue)
-parseTupKeyValue =
-    let _s = many space in do
-    _s
-    key <- parseExpr
-    _s >> char ':' >> _s
-    value <- parseExpr
-    _s >> many (char ',')
+parseTupKeyValue = do
+    key <- lexeme parseExpr
+    lexeme (char ':')
+    value <- lexeme parseExpr
+    lexeme $ many (char ',')
     return (key, value)
 
 
 parseTup :: Parser FgValue
-parseTup =
-        do
-            char '['
-            items <- try (many parseTupKeyValue) <|> many parseTupItem
-            char ']'
-            return $ Tup items
+parseTup = do
+    lexeme $ char '['
+    items <- try (many parseTupKeyValue) <|> many parseTupItem
+    lexeme $ char ']'
+    return $ Tup items
 
 
 {- BINARY OPERATORS -}
 -- Grammar X ::= Y <op> X | Y
-parseBinaryOp :: Parser FgValue -> (FgValue -> FgValue -> FgBinary) -> String -> Parser FgValue
-parseBinaryOp leftParser op tk  = do
-    let expand = do
-            left <- leftParser
-            many space
-            string tk
-            many space
-            right <- parseBinaryOp leftParser op tk
-            return $ Binary (op left right)
-    try expand <|> leftParser
+leftAssociative :: Parser FgValue -> (FgValue -> FgValue -> FgBinary) -> Parser a -> Parser FgValue
+leftAssociative leftParser op token = do
+    let rightExpansion = do
+            left <- lexeme leftParser
+            lexeme token
+            right <- lexeme parseGenExpr
+            return $ Binary $ op left right
+    try rightExpansion <|> lexeme (try leftParser)
 
 -- gen_expr  ::= or_expr (..) gen_expr | or_expr
 parseGenExpr :: Parser FgValue
-parseGenExpr = parseBinaryOp parseOr ListGenerator ".."
+parseGenExpr = leftAssociative parseSimpleExpr ListGenerator (string "..")
 
 -- or_expr   ::= xor_expr (xor) or_expr | xor_expr
 parseOr :: Parser FgValue
-parseOr = parseBinaryOp parserXOR OR "or"
+parseOr = leftAssociative parserXOR OR (string "or")
 
 -- xor_expr  ::= and_expr (or) xor_expr | and_expr
 parserXOR :: Parser FgValue
-parserXOR = parseBinaryOp parserAND XOR "xor"
+parserXOR = leftAssociative parserAND XOR (string "xor")
 
 -- and_expr  ::= comp_expr (and) and_expr | expr
 parserAND :: Parser FgValue
-parserAND = parseBinaryOp parserCompExpr AND "and"
+parserAND = leftAssociative parserCompExpr AND (string "and")
 
 -- comp_expr ::= expr (== | >= | >= | != | < | >) comp_expr | expr
 parserCompExpr :: Parser FgValue
 parserCompExpr = -- WHY ???? are <, >, = sepcials?
-        try (bin LTE "<=") <|> bin LT_ "<"
-    <|> try (bin GTE ">=") <|> bin GT_ ">"
-    <|> bin NEQ "!="
-    <|> bin EQU "=="
-    where bin = parseBinaryOp parseSimpleExpr
+        bin LTE (string "<=") <|> bin LT_ (char '<')
+    <|> bin GTE (string ">=") <|> bin GT_ (char '>')
+    <|> bin NEQ (string "!=")
+    <|> bin EQU (string "==")
+    where bin = leftAssociative parseSimpleExpr
 
 -- sexpr     ::= term (+| -) sexpr | term
 parseSimpleExpr :: Parser FgValue
 parseSimpleExpr =
-        bin PLUS "+"
-    <|> bin MINUS "-"
-    where bin = parseBinaryOp parseTerm
+        bin PLUS (char '+')
+    <|> bin MINUS (char '-')
+    where bin = leftAssociative parseTerm
 
 -- term      ::= factor (* | /) term | factor
 parseTerm :: Parser FgValue
 parseTerm =
-        bin MULT "*"
-    <|> bin DIV "/"
-    where bin = parseBinaryOp parseFactor
+        bin MULT (char '*')
+    <|> bin DIV (char '/')
+    where bin = leftAssociative parseFactor
 
--- factor    ::= (gen_expr) | unary
+
+
+
+parseParenth :: Parser FgValue
+parseParenth = do
+    lexeme (char '(')
+    expr <- parseGenExpr
+    lexeme (char ')')
+    return expr
+
 parseFactor :: Parser FgValue
-parseFactor =
-    do
-    let parenth = do
-            char '('
-            many space
-            expr <- parseGenExpr
-            many space
-            char ')'
-            return expr
-    parenth <|> parseUnary
+parseFactor = try parseParenth <|> parseUnary
 
 
-parseBinary :: Parser FgValue
-parseBinary = parseFactor
 
 {- UNARY OPERATORS / OPERANDS -}
 
-parseUnaryOp :: (FgValue -> FgUnary) -> String -> Parser FgValue
-parseUnaryOp op tk = string tk >> many space >> Unary . op <$> parseExpr
+parseUnarySpacedOp :: (FgValue -> FgUnary) -> String -> Parser FgValue
+parseUnarySpacedOp op tk = string tk >> space >> Unary . op <$> parseFactor
+
+parseUnaryNegative :: Parser FgValue
+parseUnaryNegative = char '-' >> many space >> Unary . Negative <$> parseFactor
 
 parseUnary :: Parser FgValue
-parseUnary = parseUnaryOp ReprOf "repr_of"
-    <|> parseUnaryOp NOT "not"
-    <|> parseUnaryOp Negative "-"
+parseUnary = lexeme $ parseUnarySpacedOp ReprOf "repr_of"
+    <|> parseUnarySpacedOp NOT "not"
+    <|> parseUnaryNegative
     <|> parseLiteral
     <|> parseString
     <|> parseNumber
@@ -179,10 +171,11 @@ parseUnary = parseUnaryOp ReprOf "repr_of"
 
 {- EXPRESSION -}
 parseExpr :: Parser FgValue
-parseExpr = parseFactor <|> parseUnary
--- parseExpr = parseUnary
-
-
+parseExpr = try whitespace >> (
+            try parseParenth
+        <|> try parseGenExpr
+        <|> parseUnary
+    )
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "unexpected token!" input of
--- tests/Main.hs
+++ tests/Main.hs
@@ -7,25 +7,34 @@ import qualified System.Exit as Exit
 
 tests :: Test
 tests = TestList [
-        TestCase (assertEqual "parse string" "String \"Hello\"" (readExpr "\"Hello\"")),
-        TestCase (assertEqual "parse simple number" "Unary (Negative (Unary (Negative (Tup []))))" (readExpr "--[]")),
+        TestCase (assertEqual "parse string" "String \"Hello\"" (readExpr "\"Hello\"  ")),
+        TestCase (assertEqual "parse simple number" "Unary (Negative (Unary (Negative (Tup []))))" (readExpr " - - [  ]")),
         TestCase (
             assertEqual "parse unary operator"
                 "Unary (Negative (Unary (NOT (Unary (NOT (Tup [(Number 0.0,Number 1.0)]))))))"
-                (readExpr "-  not not [1]")
+                (readExpr "- not not [1]")
         ),
-        TestCase (assertEqual "parse decimal number" "Number 213.001701" (readExpr "213.001701")),
+        TestCase (assertEqual "parse decimal number" "Number 213.001701" (readExpr "  213.001701")),
         TestCase (assertEqual "empty tup" "Tup []" (readExpr "[]")),
         TestCase (
             assertEqual "nested tup"
                 "Tup [(Number 0.0,Number 1.0),(Number 0.0,Tup [(Literal \"a\",Number 2.0),(Literal \"b\",Number 3.0)]),(Number 0.0,String \"three\")]"
                 (readExpr "[1, [ a   :2,b  : 3], \"three\"]")
         ),
+
+        TestCase (
+            assertEqual "simple expression"
+                "Binary (PLUS (Number 1.0) (Binary (MULT (Unary (Negative (Number 2.0))) (Binary (MULT (Number 3.0) (Binary (PLUS (Literal \"x\") (Number 1.0))))))))"
+                -- (readExpr "1 + -2 * 3 * (x - 1)") -- no?
+                (readExpr "1   + -2 * 3   * (x + 1)")
+        ),
+
         -- TestCase (
         --     assertEqual "parse unary operator"
-        --         ""
-        --         (readExpr "-1 .. 4 - x * 3")
+        --         "Binary (PLUS (Number 8.0) (Binary (PLUS (Number 5.0) (Unary (Negative (Binary (PLUS (Unary (NOT (Unary (NOT (Unary (NOT (Unary (Negative (Number 4.0))))))))) (Number 8.0))))))))"
+        --         (readExpr "repr_of (x + y) == 5*-(not (not not -4) + 8) ")
         -- ),
+
         TestCase (
             assertEqual "nested tup"
                 "Tup [(Number 0.0,Number 1.0),(Number 0.0,Tup [(Literal \"a\",Number 2.0),(Literal \"b\",Number 3.0)]),(Number 0.0,String \"three\")]"
